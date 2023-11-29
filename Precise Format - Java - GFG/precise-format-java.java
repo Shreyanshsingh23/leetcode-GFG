2b190844b7e8469c37f13520115b0b157db8b6b6
Shreyanshsingh23 <149963387+Shreyanshsingh23@users.noreply.github.com>
2023-11-29 21:53:51 +0530

    Added solution - LeetHub

--- Precise Format - Java - GFG/precise-format-java.java
+++ Precise Format - Java - GFG/precise-format-java.java
@@ -15,8 +15,8 @@ class Geeks{
     
     static void printInFormat(float a, float b){
         float result = a/b;
-       System.out.print(result + " " );
-       System.out.printf("%.3f", result);
+       System.out.printf(result + " " + "%.3f", result );
+       
         
         // Your code here to print upto 3 decimal places
         
