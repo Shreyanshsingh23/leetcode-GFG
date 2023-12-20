224636a43f24dcd0b614d135821d896ccf279b70
Shreyansh Singh Gautam <149963387+Shreyanshsingh23@users.noreply.github.com>
2023-12-21 01:26:43 +0530

    Time: 2 ms (66.67%), Space: 41.8 MB (6.21%) - LeetHub

---
+++ 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int countElements(int[] nums) {
+        int count = 0;
+        boolean bool1 , bool2 ;
+        for(int e : nums){
+              bool1  = false;
+            bool2 = false;
+            for(int i = 0 ; i < nums.length ; i++){
+                bool1 = nums[i] < e ;
+                if(bool1){
+                    break;
+                }
+            }
+            if(bool1){
+                for(int j = 0 ; j < nums.length ; j++){
+                    bool2 = nums[j] > e;
+                    if(bool2){
+                        break;
+                    }
+                }
+            }
+         if(bool1 == true&& bool2==true){
+             count ++;
+         }   
+        }
+        return count;
+    }
+}
