c03da2cb2827bb8a6f86fda429a4a58888736c3e
Shreyansh Singh Gautam <149963387+Shreyanshsingh23@users.noreply.github.com>
2025-01-13 12:15:25 +0530

    Time: 3 ms (69.52%) | Memory: 16.1 MB (22.04%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -13,10 +13,11 @@ public:
         if(l > n or r > n or r > l){
             return;
         }
-
-
+        if(l < n)
         rec(n,s+'(', l+1, r);
+        if(r < n and r < l)
         rec(n,s+')', l , r+1);
+
     }
 
     vector<string> generateParenthesis(int n) {
