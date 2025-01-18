4714f865cde0c81ed074ffbdc1c104e37e7bf97d
Shreyansh Singh Gautam <149963387+Shreyanshsingh23@users.noreply.github.com>
2025-01-18 22:11:37 +0530

    Time: 4 ms (62.41%) | Memory: 102.6 MB (27.80%) - LeetSync

--- 463-island-perimeter/island-perimeter.cpp
+++ 463-island-perimeter/island-perimeter.cpp
@@ -1,24 +1,21 @@
 # define pb push_back
 class Solution {
 public:
-
-    vector<int> ans;
-    set<pair<int,int>> st;
-    void rec(vector<vector<int>>& a,int i , int j){
+    int ans = 0;
+    void rec(vector<vector<int>>& a,int i , int j, int& ans){
         int n = a.size(), m = a[0].size();
-        if(i < 0 or i >= a.size() or j < 0 or j >= a[0].size()){
-            ans.pb(1);
+        if(i < 0 or i >= a.size() or j < 0 or j >= a[0].size() or a[i][j] == 0){
+            ++ans;
             return;
         }
-        if(a[i][j] == 0){
-            ans.pb(1);
+        if(a[i][j] == -1){
             return;
         }
-        st.insert({i,j});
-        if(!st.count({i+1,j})) rec(a,i+1,j);
-        if(!st.count({i-1,j})) rec(a,i-1,j);
-        if(!st.count({i,j+1})) rec(a,i,j+1);
-        if(!st.count({i,j-1})) rec(a,i,j-1);
+        a[i][j] = -1;
+         rec(a,i+1,j,ans);
+         rec(a,i-1,j,ans);
+         rec(a,i,j+1,ans);
+         rec(a,i,j-1,ans);
     }
     int islandPerimeter(vector<vector<int>>& grid) {
         int x,y;
@@ -30,7 +27,7 @@ public:
                 }
             }
         }
-        rec(grid,x,y);
-        return ans.size();
+        rec(grid,x,y,ans);
+        return ans;
     }
 };
