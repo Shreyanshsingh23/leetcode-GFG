96c5b7a26474dcad17b1e067473b3b2063eab1b4
Shreyansh Singh Gautam <149963387+Shreyanshsingh23@users.noreply.github.com>
2025-01-18 22:39:52 +0530

    Time: 25 ms (73.56%) | Memory: 16.2 MB (85.38%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     void bfs(vector<vector<char>>& a, int i, int j){
         int n = a.size(), m = a[0].size();
-        if(i >= n or i < 0 or j >= m or j < 0 or a[i][j] == 'x' or a[i][j] == '0')return;
+        if(a[i][j] == 'x' or a[i][j] == '0')return;
 
         a[i][j] = 'x';
-        bfs(a,i+1,j); 
-        bfs(a,i-1,j); 
-        bfs(a,i,j+1); 
-        bfs(a,i,j-1); 
+       if(i+1 < n) bfs(a,i+1,j); 
+       if(i > 0) bfs(a,i-1,j); 
+       if(j+1 < m) bfs(a,i,j+1); 
+       if(j > 0) bfs(a,i,j-1); 
     }
     int numIslands(vector<vector<char>>& a) {
         int cnt = 0;
