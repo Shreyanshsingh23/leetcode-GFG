1983c0140a1795543230b86c13e1bfda4d114b09
Shreyansh Singh Gautam <149963387+Shreyanshsingh23@users.noreply.github.com>
2025-02-27 09:54:10 +0530

    Time: 848 ms (15.47%) | Memory: 9.1 MB (17.44%) - LeetSync

---
+++ 1418-fair-distribution-of-cookies/fair-distribution-of-cookies.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> ans;
+    int f(vector<int>& a, int k, int i)
+    {
+        int n = a.size();
+        if(i == n){
+            return *max_element(ans.begin(),ans.end());
+        }
+        int res = 1e9;
+        for(int j = 0; j < k; ++j)
+        {
+            ans[j] += a[i];
+            res = min(res,f(a,k,i+1));
+            ans[j] -= a[i];
+        }
+
+        return res;
+    }
+    int distributeCookies(vector<int>& a, int k) {
+        ans.resize(k,0);
+
+        return f(a,k,0);
+    }
+};
