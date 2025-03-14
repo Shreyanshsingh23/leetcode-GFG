cddd1ca054a55970afd569a0a42884fb94d2e0b1
Shreyansh Singh Gautam <149963387+Shreyanshsingh23@users.noreply.github.com>
2025-03-15 00:19:43 +0530

    Time: 0 ms (100.00%) | Memory: 25.1 MB (25.68%) - LeetSync

--- 513-find-bottom-left-tree-value/find-bottom-left-tree-value.cpp
+++ 513-find-bottom-left-tree-value/find-bottom-left-tree-value.cpp
@@ -18,14 +18,12 @@ public:
         q.push(root);
         int ans = 0;
         while(!q.empty()){
-            int n = q.size();
-            for(int i = 0; i < n; ++i){
                 auto cur = q.front();
                 q.pop();
                 ans = cur->val;
                 if(cur->right)q.push(cur->right);
                 if(cur->left)q.push(cur->left);
-            }
+            
         }
         
         return ans;
