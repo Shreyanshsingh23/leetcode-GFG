343067cdb511becaeff4d8a62a4a9f69dd49baef
Shreyansh Singh Gautam <149963387+Shreyanshsingh23@users.noreply.github.com>
2025-03-22 01:18:41 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (76.15%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,29 +1,8 @@
 class Solution {
 public:
 
-    int dp[101][2][2];
-    int f(vector<int>& a, int i, bool prev, bool first)
-    {
-        const int n = a.size();
-        if(i == n)return 0;
-
-        int& ans = dp[i][prev][first];
-        if(ans != -1)return ans;
-        // not take
-        int ans1 = f(a,i+1,false,first);
-
-        // take if possible
-        bool canTake = prev? false:true;
-        if(i == n-1)canTake &= !first;
-        int ans2 = 0;
-        if(canTake){
-            ans2 = a[i] + f(a,i+1,true,first or i == 0);
-        }
-        ans = max(ans1, ans2);
-        return ans;
-        
-    }
-
+    int dp[2][2][2];
+    
     int rob(vector<int>& a) {
 
         const int n = a.size();
@@ -31,14 +10,14 @@ public:
         for(int i = n; i >= 0; --i){
             for(int prev = 1; prev >= 0; --prev){
                 for(int first = 1; first >= 0; --first){
-                    int& ans = dp[i][prev][first];
+                    int& ans = dp[i&1][prev][first];
 
                     if(i == n){
                         ans = 0;
                         continue;
                     }
                     
-                    int ans1 = dp[i+1][false][first];
+                    int ans1 = dp[(i+1)&1][false][first];
 
                     bool canTake = prev? false:true;
                     if(i == n-1){
@@ -47,7 +26,7 @@ public:
 
                     int ans2 = 0;
                     if(canTake){
-                        ans2 = a[i] + dp[i+1][true][first or i == 0];
+                        ans2 = a[i] + dp[(i+1)&1][true][first or i == 0];
                     }
                     ans = max(ans1,ans2);
                 }
