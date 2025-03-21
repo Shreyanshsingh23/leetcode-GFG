cf13af07211163984bf81481276276456b2422e1
Shreyansh Singh Gautam <149963387+Shreyanshsingh23@users.noreply.github.com>
2025-03-22 01:07:37 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (97.32%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
-    int dp[500][2];
-    bool done[500];
-    int f(int i, vector<int>& a,int k){
-        int n = a.size();
-        if(i>= n)return 0;
 
-       if(i == n-1){
-        if(k == 0)return 0;
-        else return a[i];
-       }
+    int dp[101][2][2];
+    int f(vector<int>& a, int i, bool prev, bool first)
+    {
+        const int n = a.size();
+        if(i == n)return 0;
+
+        int& ans = dp[i][prev][first];
+        if(ans != -1)return ans;
+        // not take
+        int ans1 = f(a,i+1,false,first);
+
+        // take if possible
+        bool canTake = prev? false:true;
+        if(i == n-1)canTake &= !first;
+        int ans2 = 0;
+        if(canTake){
+            ans2 = a[i] + f(a,i+1,true,first or i == 0);
+        }
+        ans = max(ans1, ans2);
+        return ans;
         
-        if(dp[i][k] != -1)return dp[i][k];
-
-        //don't take
-        int ans = f(i+1,a,k);
-
-        //take
-        done[i] = 1;
-        ans = max(ans,f(i+2,a,k)+a[i]);
-        done[i] = 0;
-
-        return dp[i][k] = ans;
-
     }
+
     int rob(vector<int>& nums) {
         memset(dp,-1,sizeof(dp));
-
-        return max(f(2,nums,0)+nums[0],f(1,nums,1));
+        
+        return f(nums,0,0,0);
     }
 };
