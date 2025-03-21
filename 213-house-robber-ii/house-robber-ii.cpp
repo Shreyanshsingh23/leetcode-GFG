d202c237e74c9e4db9d397216067c31cbc7e7a74
Shreyansh Singh Gautam <149963387+Shreyanshsingh23@users.noreply.github.com>
2025-03-22 01:17:51 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (65.75%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -24,9 +24,36 @@ public:
         
     }
 
-    int rob(vector<int>& nums) {
-        memset(dp,-1,sizeof(dp));
-        
-        return f(nums,0,0,0);
+    int rob(vector<int>& a) {
+
+        const int n = a.size();
+
+        for(int i = n; i >= 0; --i){
+            for(int prev = 1; prev >= 0; --prev){
+                for(int first = 1; first >= 0; --first){
+                    int& ans = dp[i][prev][first];
+
+                    if(i == n){
+                        ans = 0;
+                        continue;
+                    }
+                    
+                    int ans1 = dp[i+1][false][first];
+
+                    bool canTake = prev? false:true;
+                    if(i == n-1){
+                        canTake &= !first;
+                    }
+
+                    int ans2 = 0;
+                    if(canTake){
+                        ans2 = a[i] + dp[i+1][true][first or i == 0];
+                    }
+                    ans = max(ans1,ans2);
+                }
+            }
+        }
+            return dp[0][0][0];
+
     }
 };
