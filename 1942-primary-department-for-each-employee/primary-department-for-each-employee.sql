4b14c0146938b08c9d438ff49cb982cd7b806b0c
Shreyansh Singh Gautam <149963387+Shreyanshsingh23@users.noreply.github.com>
2025-05-27 00:01:49 +0530

    Time: 573 ms (45.58%) | Memory: 0B (100.00%) - LeetSync

--- 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -1,11 +1,6 @@
 select employee_id, department_id
 from Employee 
-where (employee_id, department_id) in
-                        (
-                            select employee_id, department_id
-                            from Employee
-                            where primary_flag = "Y"
-                        )
+where primary_flag = "Y"
 or (employee_id, department_id) in 
                         (
                             select employee_id, department_id
