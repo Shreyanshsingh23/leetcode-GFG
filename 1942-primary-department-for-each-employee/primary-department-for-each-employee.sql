7550eeefedcfcccc15f876fad5c9dfcab5da7e2d
Shreyansh Singh Gautam <149963387+Shreyanshsingh23@users.noreply.github.com>
2025-05-26 23:59:14 +0530

    Time: 515 ms (91.89%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -0,0 +1,16 @@
+select employee_id, department_id
+from Employee 
+where (employee_id, department_id) in
+                        (
+                            select employee_id, department_id
+                            from Employee
+                            where primary_flag = "Y"
+                        )
+or (employee_id, department_id) in 
+                        (
+                            select employee_id, department_id
+                            from Employee
+                            group by employee_id
+                            having count(department_id) = 1
+                        )
+
