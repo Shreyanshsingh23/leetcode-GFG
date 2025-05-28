46f39675a7c22a270db3b7154f44217cc2a7fbc3
Shreyansh Singh Gautam <149963387+Shreyanshsingh23@users.noreply.github.com>
2025-05-28 11:53:56 +0530

    Time: 325 ms (75.19%) | Memory: 119 MB (76.86%) - LeetSync

---
+++ 493-reverse-pairs/reverse-pairs.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> 
+#include <ext/pb_ds/tree_policy.hpp> 
+using namespace __gnu_pbds; 
+template <typename T>
+class OrderedSet {
+private:
+    tree< T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update> st;
+public:
+    int countElementsLessThanX(T x) {
+        return st.order_of_key(x);
+    }
+
+    int countElementsAtleastX(T x) {
+        return st.size() - countElementsLessThanX(x);
+    }
+
+    T getKthElement(int k) {
+        return *st.find_by_order(k);
+    }
+    
+    void insert(T x) {
+        st.insert(x);
+    }
+    
+    void erase(T x) {
+        st.erase(x);
+    }
+};
+#define ll long long
+class Solution {
+public:
+    int reversePairs(vector<int>& a) {
+        OrderedSet<pair<ll,ll>> st;
+        int ans = 0;
+        for(int i = 0; i < a.size(); ++i)
+        {
+            ans += st.countElementsAtleastX({2ll*a[i],1e9});
+            st.insert({a[i],i});
+        }
+        return ans;
+    }
+};
